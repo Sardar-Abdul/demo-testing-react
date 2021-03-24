72af32430ed0089a5e167e4b58a9dae776b17fe0
Sardar-Abdul <affixalabdullah@gmail.com>
2021-03-24 22:29:58 +0500

    final commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ test('testing app component', () => {
   const element = getByText(/Hello World/)
   expect(element).toBeInTheDocument();
 })
+
+
+test('testing app component', () => {
+  const { getByText } = render(<App />)
+  const element = getByText(/Hello Sir/)
+  expect(element).toBeInTheDocument();
+})
